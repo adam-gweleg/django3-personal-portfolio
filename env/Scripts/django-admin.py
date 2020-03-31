24e407afd6fa7652b1215a228c7a910657d6458a
Adam Gweleg <adam.jueleg@gmail.com>
2020-03-31 17:02:53 +0200

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\python projects\django 3 projects\personal_portfolio-project\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
